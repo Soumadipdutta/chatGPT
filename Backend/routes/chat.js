ceb6884783f74954ba077f514718bd047404d1f5
SoumadipDutta <soumadipdutta250@gmail.com>
2026-01-16 19:02:45 +0530

    Add Project Files

---
+++ Backend/routes/chat.js
@@ -0,0 +1,99 @@
+import express from 'express';
+import Thread from '../models/Thread.js';
+import getOpenAIAPIResponse from '../utils/openai.js';
+const router = express.Router();
+
+//test
+router.post("/test", async(req, res) => {
+    try{
+        const thread = new Thread({
+            threadId: "abc",
+            title: "Testing New Thread",
+        });
+
+        const response = await thread.save();
+        res.send(response);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: "Failed to save in DB"});
+    }
+});
+
+//Get all threads
+router.get("/thread", async(req, res) => {
+    try{
+        const threads = await Thread.find({}).sort({updatedAt: -1});
+        //descending order of updatedAt... most recent data on the top
+        res.json(threads);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: "Failed to fetch the threads"});
+    }
+})
+
+router.get("/thread/:threadId", async(req,res) => {
+    const {threadId} = req.params;
+    try{
+        const thread = await Thread.findOne({threadId});
+
+        if(!thread) {
+            res.status(404).json({error: "Thread not found"});
+        }
+
+        res.json(thread.messages);
+    }catch(err) {
+        console.log(err);
+        res.status(500).json({error: "Failed to fetch chat"});
+    }
+})
+
+router.delete("/thread/:threadId", async(req,res) => {
+    const {threadId} = req.params;
+    try{
+        const deletedThread = await Thread.findOneAndDelete({threadId});
+
+        if(!deletedThread) {
+            return res.status(404).json({error: "Thread not found"});
+        }
+
+        res.status(200).json({message: "Thread deleted successfully"});
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: "Failed to delete the thread"});
+    }
+})
+
+router.post("/chat", async(req,res) => {
+    const {threadId, message} = req.body;
+
+    if(!threadId || !message) {
+        res.status(400).json({error: "Missing required fields"});
+    }
+
+    try{
+        let thread = await Thread.findOne({threadId});
+
+        if(!thread) {
+            //create a new Thread
+            thread = new Thread({
+                threadId,
+                title: message,
+                messages: [{role: "user", content: message}]
+            });
+        } else {
+            thread.messages.push({role: "user", content: message});
+        }
+
+        const assistantReply = await getOpenAIAPIResponse(message);
+
+        thread.messages.push({role: "assistant", content: assistantReply});
+
+        thread.updatedAt = Date.now();
+        await thread.save();
+        res.json({reply: assistantReply});
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: "something went wrong"});
+    }
+});
+export default router;
